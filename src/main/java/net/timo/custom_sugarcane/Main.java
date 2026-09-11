2922af3c0674e62b581a8d40219389d4fbade1f2
Timothy <timo.dev96@gmail.com>
2026-09-11 05:31:44 +0300

    Created Logger,Mod_id and starting to make a new item

--- src/main/java/net/timo/custom_sugarcane/Main.java
+++ src/main/java/net/timo/custom_sugarcane/Main.java
@@ -1,9 +1,15 @@
 package net.timo.custom_sugarcane;
 
 import net.fabricmc.api.ModInitializer;
+import net.timo.custom_sugarcane.item.ModItems;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class Main implements ModInitializer {
+	public static final String MOD_ID = "custom_sugarcane";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/timo/custom_sugarcane/item/ModItems.java
+++ src/main/java/net/timo/custom_sugarcane/item/ModItems.java
@@ -1,4 +1,22 @@
 package net.timo.custom_sugarcane.item;
 
+import net.timo.custom_sugarcane.Main;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
 public class ModItems {
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(Main.MOD_ID, name), function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Main.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        Main.LOGGER.info("Registering ModItems" + Main.MOD_ID);
+    }
 }
