3401f0eec4531cf14ca9dbcbd43b58aa4eb3e7f0
Timothy <timo.dev96@gmail.com>
2026-09-12 11:43:25 +0300

    Change the templete
    there was a problem with Datagen gradle task so my best chice is to wipe the old template and download a new one

---
+++ src/main/java/net/timo/customsugarcane/CustomSugarcane.java
@@ -0,0 +1,22 @@
+package net.timo.customsugarcane;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomSugarcane implements ModInitializer {
+	public static final String MOD_ID = "custom-sugarcane";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
